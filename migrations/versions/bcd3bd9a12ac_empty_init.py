c3ff3eaca4eab67dc2dd57a4606709677cea20d6
telecomshy <18907190693@189.cn>
2023-04-18 17:25:14 +0800

    将多个数据库改为单个数据库

---
+++ migrations/versions/bcd3bd9a12ac_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: bcd3bd9a12ac
+Revises: 
+Create Date: 2023-04-18 17:04:35.612493
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bcd3bd9a12ac'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
