055fdbfd2111ad4d63bcc6092044458877e8aa4a
telecomshy <telecomshy@189.cn>
2023-05-10 23:49:50 +0800

    添加无验证码登录方式用于openapi

--- backend/core/config.py
+++ backend/core/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     database_url: str = "sqlite:///backend/app.db"
     secret_key: str = "b89337b4d4383e4e33175824239f82f6961282323af21862caaa94a80b0ca8a7"
     algorithm: str = "HS256"
-    access_token_expire_minutes: int = 1
+    access_token_expire_minutes: int = 15
     allowed_cors_origins: set[AnyUrl] = ["http://127.0.0.1:5173", "http://localhost:5173"]
 
 
--- backend/core/dependencies.py
+++ backend/core/dependencies.py
@@ -1,11 +1,13 @@
-from typing import Never
+from datetime import datetime, timedelta
+from typing import TypeVar
 from fastapi import Depends, status
-from fastapi.security import OAuth2PasswordBearer
-from fastapi.exceptions import HTTPException
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
-from backend.db.base import SessionDB
 from backend.core.config import settings
+from backend.core.utils import verify_password
+from backend.core.exceptions import HTTPException
+from backend.db.base import SessionDB
 from backend.db.crud.users import get_user_by_username, get_user_permissions
 from backend.db.models.users import User, Role, Menu
 
@@ -28,15 +30,35 @@ def get_token_payload(token: str = Depends(oauth2_scheme)) -> dict:
     except JWTError:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Token过期或解析失败",
+            reason="Token过期或解析失败",
             headers={"WWW-Authenticate": "Bearer"}
         )
+
+
+def authenticate_user(
+        db: Session = Depends(get_db),
+        form_data: OAuth2PasswordRequestForm = Depends()
+) -> dict:
+    """登录认证"""
+
+    username, password = form_data.username, form_data.password
+    user_db = get_user_by_username(db, username)
+
+    if user_db is None or not verify_password(password, user_db.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, reason="用户名或密码错误")
+
+    access_token_expires = datetime.utcnow() + timedelta(minutes=settings.access_token_expire_minutes)
+    # 过期时间添加了会自动生效
+    payload = {"username": username, "exp": access_token_expires}
+    access_token = jwt.encode(payload, settings.secret_key, algorithm=settings.algorithm)
+    # 返回json对象给前端，除了token，还包含前端需要的其它信息
+    return {"access_token": access_token, "username": username}
 
 
 def get_current_user(db: Session = Depends(get_db), payload: dict = Depends(get_token_payload)) -> User:
     get_current_user_error = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="无法获取当前用户"
+        reason="无法获取当前用户"
     )
 
     username = payload.get("username")
@@ -59,7 +81,6 @@ def get_current_user_menu(user_db: Depends(get_current_user)) -> list[Menu]:
         for menu in role.menus:
             menus.add(menu)
     return list(menus)
-
 
 # class RequirePermissions:
 #     def __init__(self, permissions: list[str]):
