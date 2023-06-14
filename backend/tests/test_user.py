f14b48fdd453965047f39ccbb8cceb3af3e3f6c8
telecomshy <telecomshy@189.cn>
2023-06-14 23:47:17 +0800

    测试查询用户接口

--- backend/tests/test_user.py
+++ backend/tests/test_user.py
@@ -1,8 +1,8 @@
 def test_get_users(client, token):
-    users = client("/users", headers={"Authorization": f"Bearer {token}"})
-    assert "test_user1" in [user.username for user in users]
+    users = client("/users?page=0&page_size=10", headers={"Authorization": f"Bearer {token}"})
+    assert "test_user1" in [user["username"] for user in users]
 
 
-def test_get_users_total(client):
-    total = client("/users-total")
+def test_get_users_total(client, token):
+    total = client("/users-total", headers={"Authorization": f"Bearer {token}"})
     assert total > 0, total
